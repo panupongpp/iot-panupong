df2332b2c844041c82f0edf516f3b4ce9cf378a4
panupong <panupong@DESKTOP-LEL19NI.com>
2021-09-16 19:53:42 +0700

    OK

--- java.js
+++ java.js
@@ -1,1 +1,22 @@
-let name =prompt("What your name")
+//let name =prompt("What your name")
+document.addEventListener('DOMContentLoaded',() => {
+    const submit = document.querySelector('#submit');
+    submit.disabled = true;
+    document.querySelector('#task').onkeyup = function (){
+        if(document.querySelector('#task').value.length > 0 ){
+            submit.disabled = false;
+        }
+        else{
+            submit.disabled = true;
+        }
+    }
+    document.querySelector('form').onsubmit = function (){
+        const task = document.querySelector('#task').value;
+        const li = document.createElement('li');
+        li.innerHTML = task;
+        document.querySelector('#tasks').append(li);
+        document.querySelector('#task').value ='';
+        submit.disabled=true;
+        return false;
+    }
+});
