d2fe608200af7b45f574f47ac62e3980bde18a53
panupong <panupong@DESKTOP-LEL19NI.com>
2021-09-17 21:06:39 +0700

    OK

--- show.js
+++ show.js
@@ -1,19 +1,19 @@
 //let dicountButton1= document.getElementById('show');
-function hello (){
-    if(show.innerHTML === 'Hello!'){
-        show.innerHTML = 'GoodBye';
+function Hobbies (){
+    if(show.innerHTML === '1.Music'){
+        show.innerHTML = '2.Exercise';
     }
-        else if (show.innerHTML === 'GoodBye'){
-            show.innerHTML = 'Bye';
+        else if (show.innerHTML === '2.Exercise'){
+            show.innerHTML = '3.Movies';
         }
-        else if (show.innerHTML === 'Bye'){
-            show.innerHTML = 'Bye1';
+        else if (show.innerHTML === '3.Movies'){
+            show.innerHTML = '4.Investment';
         }
-        else if (show.innerHTML === 'Bye1'){
-            show.innerHTML = 'Bye2';
+        else if (show.innerHTML === '4.Investment'){
+            show.innerHTML = '5.Travel';
         }
     else {
-        show.innerHTML = 'Hello!';
+        show.innerHTML = '1.Music';
     }
 }
 //dicountButton1.addEventListener('click', hello);
