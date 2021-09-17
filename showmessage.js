5a523f5643e534e66be9d9d4c942f18a1c51f370
panupong <panupong@DESKTOP-LEL19NI.com>
2021-09-17 21:07:10 +0700

    OK

--- showmessage.js
+++ showmessage.js
@@ -4,10 +4,10 @@ let message1 =document.getElementById('message1');
 let message2 =document.getElementById('message2');
 let message3 =document.getElementById('message3');
 function showMessage(){
-    message.innerHTML = '1.PLC' ;
-    message1.innerHTML = '2.' ;
-    message2.innerHTML = '3.' ;
-    message3.innerHTML = '4.' ;
+    message.innerHTML = '1.PLC Siemens and PLC Mitsubishi' ;
+    message1.innerHTML = '2.Internet of Things' ;
+    message2.innerHTML = '3.Control Valve and Actuator' ;
+    message3.innerHTML = '4.Instrumentation System' ;
 
 
 }
