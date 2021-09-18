05ce885affa9498cf2a9ec3f59890211d38bd05a
panupong <panupong@DESKTOP-LEL19NI.com>
2021-09-18 21:06:11 +0700

    OK

--- arryandcounter.js
+++ arryandcounter.js
@@ -1,5 +1,5 @@
 let counter = 1;
-const arry1 =["OK","OK1","OK2"]
+const arry1 =["OK","basketball","badminton","table tennis","Weight Training","ride a bike"]
 function count123(){
     if (counter >4) {
         counter=1;
@@ -10,3 +10,9 @@ function count123(){
     document.getElementById('arry').innerHTML = counter;
     document.getElementById('arrycounter').innerHTML = arry1[counter];
 }
+document.addEventListener('DOMContentLoaded',() => {
+    document.querySelector('select').onchange = function() {
+        document.getElementById('arrycounter').style.color= this.value
+        document.getElementById('arry').style.color= this.value
+    }
+});
