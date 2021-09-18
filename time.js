989b3d45f1e0c0b8bc4995cf626c4bde2ef9335f
panupong <panupong@DESKTOP-LEL19NI.com>
2021-09-18 21:04:58 +0700

    OK

---
+++ time.js
@@ -0,0 +1,29 @@
+let second = 0;
+let minute =0;
+let hour = 0;
+function time(){
+    second++;
+    if (second <60){
+    document.getElementById('time1').innerHTML =second;
+    console.log(second);
+    }
+    else if (second >59){
+        second=0;
+        document.getElementById('time1').innerHTML =second;
+    }
+}
+function time1(){
+    minute++;
+    if (minute<60){
+    document.getElementById('time2').innerHTML =minute;
+    console.log(minute);
+    }
+    else if (minute >59){
+        minute=0;
+        document.getElementById('time1').innerHTML =minute;
+    }
+}
+document.addEventListener('DOMContentLoaded', function (){
+    setInterval(time, 1000);
+    setInterval(time1, 60000);
+});
