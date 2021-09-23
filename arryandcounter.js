2950adada97df1bdc23f6c0e05d873f1a8ea366a
panupong <panupong@DESKTOP-LEL19NI.com>
2021-09-23 18:25:45 +0700

    OK

--- arryandcounter.js
+++ arryandcounter.js
@@ -11,9 +11,13 @@ function count123(){
     document.getElementById('arrycounter').innerHTML = arry1[counter];
 }
 document.addEventListener('DOMContentLoaded',() => {
+    let zxc123 =  document.getElementById('select1');
     document.querySelector('select').onchange = function() {
         document.getElementById('arrycounter').style.color= this.value
         document.getElementById('arry').style.color= this.value
         document.querySelector('select').style.background= this.value
+        if(zxc123.style.backgroundColor === "black"){
+            zxc123.style.color = "white";
+        }
     }
 });
